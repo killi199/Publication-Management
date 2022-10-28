a01644cdc3d679d3ac191858a024a8bf7aeaab8d
Kevin Jahrens - A19 <kevin.jahrens@nordakademie.de>
2022-10-28 14:57:37 +0200

    Add form

--- frontend/src/app/components/publications/publication-view/publication-view.component.ts
+++ frontend/src/app/components/publications/publication-view/publication-view.component.ts
@@ -1,14 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Publication } from 'src/app/models/publication';
 
 @Component({
-  selector: 'app-publication-view',
-  templateUrl: './publication-view.component.html',
-  styleUrls: ['./publication-view.component.scss']
+    selector: 'app-publication-view',
+    templateUrl: './publication-view.component.html',
+    styleUrls: ['./publication-view.component.scss'],
 })
 export class PublicationViewComponent {
-  @Input() publication?: Publication;
+    @Input() publication?: Publication;
 
-  constructor() { }
-
+    constructor() {}
 }
