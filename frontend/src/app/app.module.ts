79572f7ffca647ea8f1b39afe6e055329263c41f
Marcel Dymarz <marcel.dymarz@lichtblick.de>
2022-10-29 21:43:53 +0200

    some css added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -17,7 +19,7 @@ import { PublicationViewComponent } from './components/publications/publication-
         PublicationListComponent,
         PublicationViewComponent,
     ],
-    imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, MatTableModule, MatSortModule],
+    imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, MatTableModule, MatIconModule, MatSortModule, MatButtonModule],
     providers: [],
     bootstrap: [AppComponent],
 })
