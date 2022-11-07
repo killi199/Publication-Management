efda64379c79307e7e9bbcca53c130611e987c09
Kevin Jahrens - A19 <kevin.jahrens@nordakademie.de>
2022-11-07 19:52:17 +0100

    Fix Tests

--- frontend/src/app/components/publications/publication-view/publication-view.component.spec.ts
+++ frontend/src/app/components/publications/publication-view/publication-view.component.spec.ts
@@ -3,6 +3,9 @@ import { PublicationViewComponent } from './publication-view.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 describe('PublicationViewComponent', () => {
     let component: PublicationViewComponent;
@@ -10,7 +13,14 @@ describe('PublicationViewComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [FormsModule, ReactiveFormsModule, MatAutocompleteModule],
+            imports: [
+                FormsModule,
+                ReactiveFormsModule,
+                MatAutocompleteModule,
+                MatChipsModule,
+                MatDatepickerModule,
+                MatNativeDateModule,
+            ],
             declarations: [PublicationViewComponent],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
