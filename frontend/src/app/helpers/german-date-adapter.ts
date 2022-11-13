57164628668c37834518f8f057c7bcabbeb65809
Kevin Jahrens - A19 <kevin.jahrens@nordakademie.de>
2022-11-13 16:18:39 +0100

    Add provider

--- frontend/src/app/helpers/german-date-adapter.ts
+++ frontend/src/app/helpers/german-date-adapter.ts
@@ -1,21 +1,33 @@
-import { Injectable } from "@angular/core";
-import { NativeDateAdapter } from "@angular/material/core";
+import { Injectable } from '@angular/core';
+import { NativeDateAdapter } from '@angular/material/core';
 
-// Weirder stuff but it works 
+// Weirder stuff but it works
 // https://github.com/angular/components/issues/5041#issuecomment-307752968
 // https://github.com/angular/components/issues/8100
 
-@Injectable()
+@Injectable({
+    providedIn: 'root',
+})
 export class GermanDateAdapter extends NativeDateAdapter {
-  override parse(value: any): Date | null {
-    if ((typeof value === 'string') && (value.indexOf('.') > -1)) {
-      const str = value.split('.');
-      if (str.length < 2 || isNaN(+str[0]) || isNaN(+str[1]) || isNaN(+str[2])) {
-        return null;
-      }
-      return new Date(Number(str[2]), Number(str[1]) - 1, Number(str[0]), 12);
+    override parse(value: any): Date | null {
+        if (typeof value === 'string' && value.indexOf('.') > -1) {
+            const str = value.split('.');
+            if (
+                str.length < 2 ||
+                isNaN(+str[0]) ||
+                isNaN(+str[1]) ||
+                isNaN(+str[2])
+            ) {
+                return null;
+            }
+            return new Date(
+                Number(str[2]),
+                Number(str[1]) - 1,
+                Number(str[0]),
+                12
+            );
+        }
+        const timestamp = typeof value === 'number' ? value : Date.parse(value);
+        return isNaN(timestamp) ? null : new Date(timestamp);
     }
-    const timestamp = typeof value === 'number' ? value : Date.parse(value);
-    return isNaN(timestamp) ? null : new Date(timestamp);
-  }
 }
