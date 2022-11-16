d83b06235b7567adc26e1873310e48dd95321f24
Thorge Früchtenicht - A19 <thorge.fruechtenicht@nordakademie.de>
2022-11-16 19:10:17 +0100

    fixed cascade

--- backend/src/main/java/de/nordakademie/iaa/library/persistent/entities/Publication.java
+++ backend/src/main/java/de/nordakademie/iaa/library/persistent/entities/Publication.java
@@ -17,19 +17,19 @@ public class Publication {
     @NotNull
     private String title;
 
-    @OneToMany(mappedBy = "publication", fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @OneToMany(mappedBy = "publication", fetch = FetchType.LAZY, cascade = {CascadeType.ALL}, orphanRemoval = true)
     private List<AuthorsPublications> authorsPublications = new ArrayList<>();
 
     private Date dateOfPublication;
 
     private String publisher;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.ALL} )
     private KindOfPublication kindOfPublication;
 
     private String ISBN;
 
-    @OneToMany(mappedBy = "publication", fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @OneToMany(mappedBy = "publication", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<KeywordsPublications> keywordsPublications = new ArrayList<>();
 
     private int quantity;
