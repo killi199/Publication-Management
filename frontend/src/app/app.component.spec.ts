2eb107b8638fe89b120e37c05e1adde42cbf1d2c
Marcel Dymarz <marcel.dymarz@lichtblick.de>
2022-11-16 17:09:44 +0100

    fix test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -15,6 +16,7 @@ describe('AppComponent', () => {
                 MatPaginatorModule,
                 BrowserAnimationsModule,
                 MatListModule,
+                MatIconModule
             ],
             declarations: [AppComponent],
         }).compileComponents();
