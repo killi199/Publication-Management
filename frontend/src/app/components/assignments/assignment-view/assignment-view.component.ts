a11a3db3a384e0be53c86de80eebc3ce65879c3c
Kevin Jahrens - A19 <kevin.jahrens@nordakademie.de>
2022-11-17 18:33:34 +0100

    Add inputs to assignment view

--- frontend/src/app/components/assignments/assignment-view/assignment-view.component.ts
+++ frontend/src/app/components/assignments/assignment-view/assignment-view.component.ts
@@ -1,12 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
+import { Assignment } from 'src/app/models/assignment';
+import { Borrower } from 'src/app/models/borrower';
 
 @Component({
-  selector: 'app-assignment-view',
-  templateUrl: './assignment-view.component.html',
-  styleUrls: ['./assignment-view.component.scss']
+    selector: 'app-assignment-view',
+    templateUrl: './assignment-view.component.html',
+    styleUrls: ['./assignment-view.component.scss'],
 })
-export class AssignmentViewComponent {
+export class AssignmentViewComponent implements OnInit {
+    @Input()
+    assignment: Assignment = {};
 
+    @Input()
+    allBorrowers: Borrower[] = [];
 
+    formGroup = new FormGroup({
+        publicationKey: new FormControl<string>(''),
+        dateOfAssignment: new FormControl<Date>(new Date()),
+        dateOfReturn: new FormControl<Date>(new Date()),
+        borrower: new FormControl<string | Borrower>(''),
+    });
 
+    filteredBorrowers: Observable<Borrower[]> = new Observable<Borrower[]>();
+
+    ngOnInit(): void {
+        if (this.assignment) {
+            this.formGroup.disable();
+            this.formGroup.patchValue(this.assignment);
+        }
+
+        this._reloadView();
+    }
+
+    onSubmit(): void {}
+
+    displayBorrower(borrower: Borrower): string {
+        return borrower?.surname && borrower?.name
+            ? borrower.surname + ' ' + borrower.name
+            : '';
+    }
+
+    private _filterBorrowers(value: string): Borrower[] {
+        const filterValue = value.toLowerCase();
+
+        return this.allBorrowers.filter(
+            (borrower) =>
+                borrower.surname?.toLowerCase().includes(filterValue) ||
+                borrower.name?.toLowerCase().includes(filterValue)
+        );
+    }
+
+    private _reloadView(): void {
+        this.filteredBorrowers = this.formGroup
+            .get('borrower')!
+            .valueChanges.pipe(
+                startWith(''),
+                map((borrower) => {
+                    let value = '';
+
+                    if (typeof borrower === 'string') {
+                        value = borrower;
+                    } else if (borrower?.surname && borrower?.name) {
+                        value = borrower?.surname + borrower?.name;
+                    }
+
+                    return value
+                        ? this._filterBorrowers(value as string)
+                        : this.allBorrowers.slice();
+                })
+            );
+    }
 }
--- frontend/src/app/components/assignments/assignments.component.ts
+++ frontend/src/app/components/assignments/assignments.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Assignment } from 'src/app/models/assignment';
+import { Borrower } from 'src/app/models/borrower';
 import { AssignmentService } from 'src/app/services/assignment.service';
+import { BorrowerService } from 'src/app/services/borrower.service';
 
 @Component({
     selector: 'app-assignments',
     templateUrl: './assignments.component.html',
     styleUrls: ['./assignments.component.scss'],
 })
-export class AssignmentsComponent {
+export class AssignmentsComponent implements OnInit {
     assignments: Observable<Assignment[]>;
     openAssignment: boolean = false;
+    borrowers: Borrower[] = [];
     currentAssignment?: Assignment;
-    constructor(assignmentService: AssignmentService) {
+    constructor(assignmentService: AssignmentService, private borrowerService: BorrowerService) {
         this.assignments = assignmentService.loadAllAssignments();
+    }
+
+    ngOnInit(): void {
+        this.borrowerService
+            .loadAllBorrowers()
+            .subscribe((borrowers) => (this.borrowers = borrowers));
     }
 
     onBack(): void {
