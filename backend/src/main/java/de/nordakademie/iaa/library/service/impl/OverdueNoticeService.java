d58c234fe356eb8500355294b8a1e02963a46a6e
Thorge Früchtenicht - A19 <thorge.fruechtenicht@nordakademie.de>
2022-11-19 23:03:51 +0100

    performance boooost to over 9000 🚀

--- backend/src/main/java/de/nordakademie/iaa/library/service/impl/OverdueNoticeService.java
+++ backend/src/main/java/de/nordakademie/iaa/library/service/impl/OverdueNoticeService.java
@@ -2,6 +2,7 @@ package de.nordakademie.iaa.library.service.impl;
 
 import de.nordakademie.iaa.library.controller.dto.OverdueNoticeDto;
 import de.nordakademie.iaa.library.persistent.entities.OverdueNotice;
+import de.nordakademie.iaa.library.persistent.entities.Publication;
 import de.nordakademie.iaa.library.persistent.repository.OverdueNoticeRepository;
 import de.nordakademie.iaa.library.service.OverdueNoticeServiceInterface;
 import de.nordakademie.iaa.library.service.mapper.OverdueNoticeMapper;
@@ -11,7 +12,10 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * The OverdueNotice service provides methods to handle the OverdueNotices
@@ -24,10 +28,15 @@ public class OverdueNoticeService implements OverdueNoticeServiceInterface {
 
     private final OverdueNoticeMapper overdueNoticeMapper;
 
+    private final PublicationService publicationService;
+
     @Autowired
-    public OverdueNoticeService(OverdueNoticeRepository overdueNoticeRepository, OverdueNoticeMapper overdueNoticeMapper) {
+    public OverdueNoticeService(OverdueNoticeRepository overdueNoticeRepository,
+                                OverdueNoticeMapper overdueNoticeMapper,
+                                PublicationService publicationService) {
         this.overdueNoticeRepository = overdueNoticeRepository;
         this.overdueNoticeMapper = overdueNoticeMapper;
+        this.publicationService = publicationService;
     }
 
     /**
@@ -37,7 +46,34 @@ public class OverdueNoticeService implements OverdueNoticeServiceInterface {
      */
     public List<OverdueNoticeDto> getAll() {
         List<OverdueNotice> overdueNotices = overdueNoticeRepository.findAll();
-        return overdueNoticeMapper.overdueNoticeEntitiesToDtos(overdueNotices);
+        return overdueNoticeMapper.overdueNoticeEntitiesToDtos(loadPublications(overdueNotices));
+    }
+
+    /**
+     * Preload the Publications in the assignments of
+     * the overdue notices to lower the total number of requests performed.
+     *
+     * @param overdueNotices the overdueNotices that should be loaded
+     * @return List of overdueNotices
+     */
+    private List<OverdueNotice> loadPublications(List<OverdueNotice> overdueNotices) {
+        List<Publication> publications = publicationService.getAllByKeys(
+                overdueNotices
+                        .stream()
+                        .map(overdueNotice -> overdueNotice.getAssignment().getPublication().getKey())
+                        .collect(Collectors.toList()));
+
+        Map<String, Publication> publicationMap =
+                publications
+                        .stream()
+                        .collect(Collectors.toMap(Publication::getKey, Function.identity()));
+
+        for (OverdueNotice overdueNotice: overdueNotices) {
+            overdueNotice.getAssignment().setPublication(
+                    publicationMap.get(overdueNotice.getAssignment().getPublication().getKey()));
+        }
+
+        return overdueNotices;
     }
 
     /**
