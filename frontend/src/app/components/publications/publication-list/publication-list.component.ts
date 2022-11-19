cd0da46eff9bc459e6775a6e9a95a9ed7d70849c
Marcel Dymarz <marcel.dymarz@lichtblick.de>
2022-11-19 01:35:44 +0100

    easy filterspecifications added for pubs...nicht ez!

--- frontend/src/app/components/publications/publication-list/publication-list.component.ts
+++ frontend/src/app/components/publications/publication-list/publication-list.component.ts
@@ -9,9 +9,7 @@ import { Publication } from 'src/app/models/publication';
     styleUrls: ['./publication-list.component.scss'],
 })
 export class PublicationListComponent extends TableInitsComponent<Publication> implements OnInit {
-    @Input() publications: Observable<Publication[]> = new Observable<
-        Publication[]
-    >();
+    @Input() publications: Observable<Publication[]> = new Observable<Publication[]>();
 
     @Output() showPublication = new EventEmitter<Publication>();
 
@@ -29,6 +27,11 @@ export class PublicationListComponent extends TableInitsComponent<Publication> i
         'quantity',
     ];
 
+    constructor() {
+        super();
+        this._filterSpecifications();
+    }
+
     ngOnInit(): void {
         this.publications.subscribe((publications) => {
             this.dataSource.data = publications;
@@ -45,5 +48,34 @@ export class PublicationListComponent extends TableInitsComponent<Publication> i
             this.showPublication.emit(publication);
             this.selectedPublication = publication;
         }
+    }
+
+    private _filterSpecifications(): void {
+        this.dataSource.filterPredicate = (data: Publication, filter: string): boolean => {
+            const keyFilter = this._include(data.key, filter);
+            const titleFilter = this._include(data.title, filter);
+            const authorFilter = this._include(data.authors?.map((a) => '' + a.name + a.surname).join(' '), filter);
+            // TODO: iwie an das short-Date Format rankommen und dann wie gehabt zum string machen und gucken ob der filter im string steckt..
+            // const dateOfPublicationFilter = data.dateOfPublication;
+            const publisherFilter = this._include(data.publisher, filter);
+            const kindOfPubFilter = this._include(data.kindOfPublication?.value, filter);
+            const isbnFilter = this._include(data.isbn, filter);
+            const keywordFilter = this._include(data.keywords?.map((k) => k.value).join(' '), filter);
+            const quantityFilter = this._include(data.quantity?.toString(), filter);
+            return (
+                keyFilter ||
+                titleFilter ||
+                authorFilter ||
+                publisherFilter ||
+                kindOfPubFilter ||
+                isbnFilter ||
+                keywordFilter ||
+                quantityFilter
+            );
+        };
+    }
+
+    private _include(dataValue: string | undefined | null, filter: string): boolean {
+        return dataValue?.trim().toLowerCase().includes(filter) ?? false;
     }
 }
