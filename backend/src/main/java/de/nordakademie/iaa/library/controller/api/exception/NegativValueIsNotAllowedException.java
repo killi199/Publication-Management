b0893f9bec864eb48dec3f9848fa31d30a3ea1fc
Nello Musmeci - A19 <nello.musmeci@nordakademie.de>
2022-11-21 09:10:51 +0000

    fix english :)

--- backend/src/main/java/de/nordakademie/iaa/library/controller/api/exception/NegativValueIsNotAllowedException.java
+++ backend/src/main/java/de/nordakademie/iaa/library/controller/api/exception/NegativValueIsNotAllowedException.java
@@ -3,7 +3,7 @@ package de.nordakademie.iaa.library.controller.api.exception;
 import org.springframework.http.HttpStatus;
 
 /**
- * This exception is thrown if an integer value is negativ but shouldn't be.
+ * This exception is thrown if an integer value is negative but it shouldn't be.
  */
 public class NegativValueIsNotAllowedException extends AbstractRestApiException {
 
