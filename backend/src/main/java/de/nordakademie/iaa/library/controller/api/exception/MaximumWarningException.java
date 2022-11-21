12e847e044b1b29b029745b08fcba2158f169348
Thorge Früchtenicht - A19 <thorge.fruechtenicht@nordakademie.de>
2022-11-21 09:53:11 +0100

    added java doc for exceptions

--- backend/src/main/java/de/nordakademie/iaa/library/controller/api/exception/MaximumWarningException.java
+++ backend/src/main/java/de/nordakademie/iaa/library/controller/api/exception/MaximumWarningException.java
@@ -2,6 +2,9 @@ package de.nordakademie.iaa.library.controller.api.exception;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * This exception is thrown when the maximum number of warnings is reached.
+ */
 public class MaximumWarningException extends AbstractRestApiException {
 
     public MaximumWarningException() {
--- backend/src/main/java/de/nordakademie/iaa/library/controller/api/exception/OverdueNoticeIsClosedException.java
+++ backend/src/main/java/de/nordakademie/iaa/library/controller/api/exception/OverdueNoticeIsClosedException.java
@@ -2,6 +2,9 @@ package de.nordakademie.iaa.library.controller.api.exception;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * This exception will be thrown when an overdue notice is closed or does not exists.
+ */
 public class OverdueNoticeIsClosedException extends AbstractRestApiException {
 
     public OverdueNoticeIsClosedException() {
